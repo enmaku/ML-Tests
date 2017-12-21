9dff5ac2f3b051d8a3ab66cae612db6bdf5937c1
David Perry <enmaku@gmail.com>
2017-12-20 16:44:30 -0800

    WTF, 128 neurons function just as well as 1024. YU over-engineer your sample code, google?

--- tensorflow_tests.py
+++ tensorflow_tests.py
@@ -42,8 +42,8 @@ h_pool2 = max_pool_2x2(h_conv2)
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 
 # Another ReLU layer
-W_fc1 = weight_variable([7 * 7 * 64, 256])
-b_fc1 = bias_variable([256])
+W_fc1 = weight_variable([7 * 7 * 64, 128])
+b_fc1 = bias_variable([128])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # Dropout layer
@@ -51,7 +51,7 @@ keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Final (readout) layer
-W_fc2 = weight_variable([256, 10])
+W_fc2 = weight_variable([128, 10])
 b_fc2 = bias_variable([10])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
