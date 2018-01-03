0cd5cd3106c1b44ec48d5729e299ef0dc8150939
David Perry <enmaku@gmail.com>
2018-01-03 14:33:55 -0800

    More cleanup

--- keras_tests.py
+++ keras_tests.py
@@ -43,14 +43,14 @@ except:
     # Build it from scratch if it doesn't.
     print("No model found or model could not be loaded. Starting over.")
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape, name='conv1'))
-    model.add(MaxPooling2D(pool_size=(2, 2), name='maxpool1'))
-    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', name='conv2'))
-    model.add(MaxPooling2D(pool_size=(2, 2), name='maxpool2'))
-    model.add(Flatten(name='flatten'))
-    model.add(Dense(64, activation='relu', name='dense1'))
-    model.add(Dropout(0.5, name='dropout1'))
-    model.add(Dense(num_classes, activation='softmax', name='dense2'))
+    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
     model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.adam(), metrics=['accuracy'])
 
 # Set up our checkpoint saves.
