a62c5719a463eedbfa040f955041fd189b1b7651
David Perry <enmaku@gmail.com>
2018-01-04 11:43:35 -0800

    Realized keras/tf examples were not equivalent because keras layers did not have use_bias set

--- keras_mnist.py
+++ keras_mnist.py
@@ -45,14 +45,14 @@ except:
     # Build it from scratch if it doesn't.
     print("No model found or model could not be loaded. Starting over.")
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', use_bias=True, input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(64, kernel_size=(5, 5), activation='relu'))
+    model.add(Conv2D(64, kernel_size=(5, 5), activation='relu', use_bias=True))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu', use_bias=True))
     model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dense(num_classes, activation='softmax', use_bias=True))
     model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.adam(), metrics=['accuracy'])
 
 # Set up our checkpoint saves.
