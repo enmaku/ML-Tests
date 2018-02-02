baf2af6f4e397daf0f0ab34dc0cf1d6693490efb
Don Viszneki <don@codebad.com>
2018-02-02 13:27:25 -0800

    lstm_word_gen: a few improvements
    1) tokenize our corpus differently: the regexp now used probably correctly
    identifies most words and some punctuation marks. this brings the number of
    unique tokens down to around 6k, which is a huge improvement to the size of
    our one-hot token representations.
    
    2) some variables were renamed to be more specific.
    
    3) trade some cpu for a lot of RAM. previously we were filling all of our
    data into a single tensor and allowing keras to batch it for us. in this
    change, we generate new batches before each call to keras' Model.fit()
    method. notably, keras is now batching at a batch size of 32, however, we
    still feed it 128 records at a time.
    
    4) demonstrations of text generation do not happen 1:1 with calls to
    Model.fit() anymore

--- keras/lstm_word_generation.py
+++ keras/lstm_word_generation.py
@@ -9,6 +9,7 @@ import numpy as np
 import random
 import sys
 import os
+import re
 from unidecode import unidecode
 
 path = "datasets/pride_prejudice.txt"
@@ -17,54 +18,59 @@ text = unidecode(open(path, 'r', encoding='utf-8').read().lower())
 
 print('corpus length:', len(text))
 
-chars = set(text)
-words = set(open(path).read().lower().split())
+token_re = re.compile(r'(\w+|[\'])+|[.,!"]')# TODO more punctuation and more exotic chars
+# corpus_tokens should contain a list of tokens, which for the above regexp
+# should be words and some punctuation marks
+corpus_tokens = list(map(
+  lambda match: match.string[match.start():match.end()]
+, token_re.finditer(open(path).read().lower())
+))
+# Obtain a set of all unique tokens in the corpus
+token_set = set(corpus_tokens)
 
-print("chars:", type(chars))
-print("words", type(words))
-print("total number of unique words", len(words))
-print("total number of unique chars", len(chars))
+print("total number of unique tokens", len(token_set))
 
+# Assign each token an ordinal. Generate two-way mapping
+ordinals_by_token = dict((c, i) for i, c in enumerate(token_set))
+tokens_by_ordinal = dict((i, c) for i, c in enumerate(token_set))
+# Map our corpus' tokens to their ordinals
+corpus_ordinals = [ordinals_by_token[token] for token in corpus_tokens]
+# TODO del corpus_tokens ?
 
-word_indices = dict((c, i) for i, c in enumerate(words))
-indices_word = dict((i, c) for i, c in enumerate(words))
+# the length of our input sequence, or, our feature vector length
+seqlen = 30
+# our stride in batching features from our corpus
+step = 30
+# the number of predictions we'll optimize for at a time
+batch_size = 128
+# the number of times we call Keras' fit() method before demonstrating text
+# generation
+batches_per_demo = 8
+print("seqlen:", seqlen, "step:", step)
+# check that we'll get a healthy mix of sequences
+if len(corpus_ordinals) % step == 0:
+  print('WARNING: your corpus is a multiple of your step size')
 
-print("word_indices", type(word_indices), "length:", len(word_indices))
-print("indices_words", type(indices_word), "length", len(indices_word))
-
-maxlen = 30
-step = 3
-print("maxlen:", maxlen, "step:", step)
-sentences = []
-next_words = []
-list_words = text.lower().split()
-
-
-for i in range(0, len(list_words)-maxlen, step):
-    sentences2 = ' '.join(list_words[i: i + maxlen])
-    sentences.append(sentences2)
-    next_words.append((list_words[i + maxlen]))
-print('nb sequences(length of sentences):', len(sentences))
-print("length of next_word", len(next_words))
-
-print('Vectorization...')
-X = np.zeros((len(sentences), maxlen, len(words)), dtype=np.bool)
-y = np.zeros((len(sentences), len(words)), dtype=np.bool)
-for i, sentence in enumerate(sentences):
-    for t, word in enumerate(sentence.split()):
-        # print(i, t, word)
-        X[i, t, word_indices[word]] = 1
-    y[i, word_indices[next_words[i]]] = 1
+# we'll use one-hot encoding representation of tokens. this means a huge
+# bool vector for each token representation of length len(token_set)
+# NOTE unlike the previous version, we'll allocate tensors for a single
+# batch, and repopulate them for each optimization
+# training features tensor requires shape:
+#   [batch size, sequence length, token encoding]
+X = np.zeros((batch_size, seqlen, len(token_set)), dtype=np.bool)
+# training labels tensor for predicting individual words requires shape:
+#   [batch size, token encoding]
+y = np.zeros((batch_size, len(token_set)), dtype=np.bool)
 
 
 # build the model: 2 stacked LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(512, return_sequences=True, input_shape=(maxlen, len(words))))
+model.add(LSTM(512, return_sequences=True, input_shape=(seqlen, len(token_set))))
 model.add(Dropout(0.2))
 model.add(LSTM(512, return_sequences=False))
 model.add(Dropout(0.2))
-model.add(Dense(len(words)))
+model.add(Dense(len(token_set)))
 # model.add(Dense(1000))
 model.add(Activation('softmax'))
 
@@ -83,41 +89,64 @@ def sample(a, temperature=1.0):
     return np.argmax(np.random.multinomial(1, a, 1))
 
 
+# we'll do our own batching to save on memory
+batchCursor = 0
+
 # train the model, output generated text after each iteration
 for iteration in range(1, 300):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=128, epochs=2)
+
+    # populate our batch
+    X.fill(0) # should be reasonably performant
+    y.fill(0)
+    # TODO can this be optimized further?
+    for iSample in range(batch_size):
+      # one-hot for features
+      for iSeq, ordinal in enumerate(corpus_ordinals[batchCursor : batchCursor + seqlen]):
+        X[iSample, iSeq, ordinal] = 1
+      # one-hot for labels
+      ordinal = corpus_ordinals[batchCursor + seqlen]
+      y[iSample, ordinal] = 1
+      # increment batchCursor by step, wrapping around from
+      # end-to-beginning, but respecting that there should be seqlen+1
+      # additional tokens following the cursor
+      batchCursor = (batchCursor + step + seqlen + 1) % len(corpus_tokens)
+
+    model.fit(X, y, epochs=2)
     model.save_weights('lstm_character_gernation_weights.h5', overwrite=True)
 
-    start_index = random.randint(0, len(list_words) - maxlen - 1)
+    start_index = random.randint(0, len(corpus_tokens) - seqlen - 1)
 
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
-        print()
-        print('----- diversity:', diversity)
-        generated = ''
-        sentence = list_words[start_index: start_index + maxlen]
-        generated += ' '.join(sentence)
-        print('----- Generating with seed: "', sentence, '"')
-        print()
-        sys.stdout.write(generated)
-        print()
+    if iteration % batches_per_demo == 0:
+      for diversity in [0.2, 0.5, 1.0, 1.2]:
+          print()
+          print('----- diversity:', diversity)
+          generated = ''
+          sentence = corpus_tokens[start_index: start_index + seqlen]
+          generated += ' '.join(sentence)
+          print('----- Generating with seed: "', sentence, '"')
+          print()
+          sys.stdout.write(generated)
+          print()
 
-        for i in range(1024):
-            x = np.zeros((1, maxlen, len(words)))
-            for t, word in enumerate(sentence):
-                x[0, t, word_indices[word]] = 1.
+          for i in range(1024):
+              x = np.zeros((1, seqlen, len(token_set)))
+              for t, word in enumerate(sentence):
+                  x[0, t, ordinals_by_token[word]] = 1.
 
-            preds = model.predict(x, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_word = indices_word[next_index]
-            generated += next_word
-            del sentence[0]
-            sentence.append(next_word)
-            sys.stdout.write(' ')
-            sys.stdout.write(next_word)
-            sys.stdout.flush()
-        print()
+              preds = model.predict(x, verbose=0)[0]
+              next_index = sample(preds, diversity)
+              next_word = tokens_by_ordinal[next_index]
+              generated += next_word
+              del sentence[0]
+              sentence.append(next_word)
+              sys.stdout.write(' ')
+              sys.stdout.write(next_word)
+              sys.stdout.flush()
+          print()
+
+    print('batchCursor = %s' % batchCursor)
 
 model.save_weights('lstm_character_gernation_weights.h5')
